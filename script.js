a18bc501e4a091ab476a949064230b1af5191d78
bj0sin <alva.danielsson14@gmail.com>
2026-05-11 14:47:19 +0200

    scb

---
+++ script.js
@@ -0,0 +1,55 @@
+const urlSCB =
+  "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/MI/MI1303/MI1303B/ExplVatmark";
+
+const querySCB = {
+  query: [
+    {
+      code: "Region",
+      selection: {
+        filter: "vs:RegionLän07EjAggr",
+        values: [
+          "01",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "12",
+          "13",
+          "14",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+        ],
+      },
+    },
+    {
+      code: "ContentsCode",
+      selection: {
+        filter: "item",
+        values: ["000006WZ"],
+      },
+    },
+  ],
+  response: {
+    format: "JSON",
+  },
+};
+
+const request = new Request(urlSCB, {
+  method: "POST",
+  body: JSON.stringify(querySCB),
+});
+
+fetch(request)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
